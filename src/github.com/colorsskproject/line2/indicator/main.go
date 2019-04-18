5bd8e0a013492f0fbf22ee2563dc9621702ac4cc
Colorssk <37653722+Colorssk@users.noreply.github.com>
2019-04-18 18:48:59 +0800

    引用类型

--- src/github.com/colorsskproject/line2/indicator/main.go
+++ src/github.com/colorsskproject/line2/indicator/main.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 )
 
+func arrmodify(arr *[3]int) {
+	(*arr)[0] = 2
+}
+func modify(arr []int) {
+	arr[1] = 1
+}
 func testPoint() {
 	var a int = 1
 	var b *int = &a
@@ -15,5 +21,11 @@ func testPoint() {
 	fmt.Println(a)
 }
 func main() {
+	var a [3]int = [3]int{1, 2, 3}
+	arrmodify(&a)
+	fmt.Println(a)
+	// 等同于用引用类型的切片
+	modify(a[:])
+	fmt.Println(a)
 	testPoint()
 }
